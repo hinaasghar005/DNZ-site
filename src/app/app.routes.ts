bc592dea3299558889319e83b7037ab10e3327fb
sabahatyaseen15 <sabahatyaseen15@gmail.com>
2024-09-09 22:32:06 -0700

    login page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+import { Component } from '@angular/core';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: 'login', component:LoginComponent } ,
+
+];
