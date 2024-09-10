78e1ccc6a3a955883b35edb0d97923751826111e
Sidra Khalid <147918296+sidrakhalid4488@users.noreply.github.com>
2024-09-10 17:00:40 +0500

    footer done

--- src/app/Component/landing-page/landing-page.component.ts
+++ src/app/Component/landing-page/landing-page.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { FooterComponent } from '../../shared/footer/footer.component';
 
 @Component({
   selector: 'app-landing-page',
   standalone: true,
-  imports: [],
+  imports: [FooterComponent],
   templateUrl: './landing-page.component.html',
   styleUrl: './landing-page.component.css'
 })
