c780eab77648b216523b5789730cb9a847854e9b
Sidra Khalid <147918296+sidrakhalid4488@users.noreply.github.com>
2024-09-10 10:42:11 +0500

    routes  and routeroutlet

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { Component } from '@angular/core';
+import { LandingPageComponent } from './Component/landing-page/landing-page.component';
 
 export const routes: Routes = [
+    {path:'',component:LandingPageComponent},
     { path: 'login', component:LoginComponent } ,
 
 ];
