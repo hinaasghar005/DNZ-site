b198bcca85fb0951d909b86b3d9f94ed1f6ef541
sabahatyaseen15 <sabahatyaseen15@gmail.com>
2024-09-11 00:16:21 -0700

    add choose-goal component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,9 +2,12 @@ import { Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { Component } from '@angular/core';
 import { LandingPageComponent } from './Component/landing-page/landing-page.component';
+import { ChooseGoalsComponent } from './Component/choose-goals/choose-goals.component';
 
 export const routes: Routes = [
     {path:'',component:LandingPageComponent},
     { path: 'login', component:LoginComponent } ,
+    { path: 'choose-goal', component:ChooseGoalsComponent } ,
+
 
 ];
