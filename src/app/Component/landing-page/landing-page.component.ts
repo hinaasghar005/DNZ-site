d43531dc72e4dd63fed256c78cfa01cddd21c2e2
sabahatyaseen15 <sabahatyaseen15@gmail.com>
2024-10-09 00:31:56 -0700

    sign-up component

--- src/app/Component/landing-page/landing-page.component.ts
+++ src/app/Component/landing-page/landing-page.component.ts
@@ -3,11 +3,13 @@ import { FooterComponent } from '../../shared/footer/footer.component';
 import { ReviewsComponent } from '../reviews/reviews.component';
 import { FoodStoriesComponent } from '../food-stories/food-stories.component';
 import { BestNutritionistComponent } from '../best-nutritionist/best-nutritionist.component';
+import { RouterLink } from '@angular/router';
+import { LoginComponent } from '../../login/login.component';
 
 @Component({
   selector: 'app-landing-page',
   standalone: true,
-  imports: [FooterComponent,ReviewsComponent,FoodStoriesComponent,BestNutritionistComponent],
+  imports: [FooterComponent,ReviewsComponent,FoodStoriesComponent,BestNutritionistComponent,RouterLink],
   templateUrl: './landing-page.component.html',
   styleUrl: './landing-page.component.css'
 })
